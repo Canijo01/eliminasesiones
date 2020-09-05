d73f097861d2b80f8cfee5472db72e1c122e4c04
Francisco martinez <fmartinez@gigamericas.tech>
2020-09-04 19:33:55 -0500

    Ya lee en minusculas y mayusculas el config.ini

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from funciones import *
 
 ### Inicio del programa
 config = configparser.ConfigParser()
+### Esta linea es para conservar las mayusculas. Normalmente convierte a minusculas
+config.optionxform = str
+
 leer_parametros = config.read("config.ini")
 
 ### Acceso a la API
@@ -26,15 +29,18 @@ else:
 ### DAYS : Numero de dias anteriores para cargar los registros ej. 5
 ### IDLE #Seconds without open RDP apps before Awingu assumes session is idle
 ### DELAY ## Seconds between interactions ej. 60
+
+
 for secciones in config.sections():
     for variable in config.items(secciones):
+        print(variable)
         dato = variable[1]
         if variable[0] in os.environ:
             dato = os.environ[variable[0]]
         exec("%s=\"%s\"" % (variable[0], dato))
         print("%s=\"%s\"" % (variable[0], dato))
-verify=bool(verify)
-user="%s\\%s"%(domain,username)
+VERIFY=bool(VERIFY)
+user="%s\\%s"%(DOMAIN,USERNAME)
 duplicados =[]
 
 ### Headers para los restful API
@@ -43,13 +49,12 @@ headers = {
     'Authorization': "Token %s" % (token)
 }
 ### Obtener el uri del dominio
-domain_uri = getdomainuri(headers, url, domain)
-print(domain_uri)
+domain_uri = getdomainuri(headers, url, DOMAIN)
 
 ### Inicio del programa
 ### Obtener las sesiones abiertas en el dominio
 print ("Verificado no hay sesiones duplicadas para %s:"%(user))
-sessions_list = usersessionslist(headers,url, domain,verify)
+sessions_list = usersessionslist(headers,url, DOMAIN,VERIFY)
 if sessions_list.status_code == requests.codes.ok:
     #print(json.dumps(json.loads(sessions_list.text),indent=4,sort_keys=True))
     sesionesabiertas=json.loads(sessions_list.text)
@@ -64,7 +69,7 @@ if sessions_list.status_code == requests.codes.ok:
             duplicadosSorteados=sorted(duplicados,key=lambda date: datetime.datetime.strptime(date["start"] + "Z", "%Y-%m-%dT%H:%M:%S.%f%z"),reverse=1)
             for i in range (1,len(duplicadosSorteados)):
                 print ("borrando sesion:%s"% duplicadosSorteados[i]["session_id"])
-                closesession(headers,url,duplicadosSorteados[i]["session_id"],verify)
+                closesession(headers,url,duplicadosSorteados[i]["session_id"],VERIFY)
         else:
             print("Usuario % solo tiene una sesion abiertas")
 
